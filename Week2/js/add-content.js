5e0bfad3ef49eefef25024ae792d97143b599232
PrinceTheo <princecth2009@yahoo.com>
2015-11-24 09:31:38 +0700

    You are all welcome

--- Week2/js/add-content.js
+++ Week2/js/add-content.js
@@ -1,4 +1,7 @@
-document.write("Hello Week2!");
+<script type="text/javascript" src="js/add-content.js"></script>
+     <script>
+        document.write("Hello Week 2!");
+		</script>
 body{
 	background-color: grey;
 }
