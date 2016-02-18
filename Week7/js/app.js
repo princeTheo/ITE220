c7f0e2987e520d56059a297628b0258bf1248900
PrinceTheo <princecth2009@yahoo.com>
2016-02-18 12:04:03 +0700

    Welcome to the updated version of week7 weather application with comments

--- Week7/js/app.js
+++ Week7/js/app.js
@@ -5,24 +5,29 @@ $(function (){
 		var city = this.text.toLowerCase();
 		$.ajax({
 			type: 'GET',
+			// url API for open weather map that fetches city json data value in either degrees or fahrenheit
 			url: 'http://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid=9fc92320165fc94d421601aeb79d3de1',
 			success: function(data){
 				//console.log(data);
 				$("#container").html(data.name + ": ");
 				$("#container").html("");
+				// function to convert the temperature degree celsius
 				function convertC(temp){
+					//math.round function returns the value of the number rounded to the nearest integer.
 					temp = Math.round(temp - 273.15);
 					return (temp +"&deg;C");
 				};
 
+                 //also function to convert the temperature degree fahrenheit 
 				function convertF(temp){
+					//math.round function returns the value of the number rounded to the nearest integer.
 					temp = Math.round((temp * 9 / 5)-459.67);
 					return (temp + "&deg;F");
 				};
-
+                 //function that connects or links the div "container" with class "convert" id "celsius" in the html to convert and fetch the data to display on the html
 			$("#celsius").click(function(){
 					$("#container").html(convertC(data.main.temp));
-					
+				//function that connects or links the div "container" with class "convert" id "fahrenhiet" in the html to convert and fetch the data to display on the html
 				});
              $("#fahrenheit").click(function(){
 					$("#container").html(convertF(data.main.temp));
